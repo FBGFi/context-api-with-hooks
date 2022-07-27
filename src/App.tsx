ba9147e29d96340575fd56f2641faa78aadbec1e
Aleksi Alhola <aleksi.alhola@valmetpartners.com>
2022-07-27 15:59:05 +0300

    tasks 1 and 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Main } from './task1/Main';
+// import { Main } from './task2/Main';
+// import { Main } from './solution1/Main';
+import { Main } from './solution2/Main';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Main/>;
 }
 
 export default App;
